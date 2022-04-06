77fcea273265b37f638568749c9e4b0303c429d0
Anuj Singh <101380771+anuj1010@users.noreply.github.com>
2022-04-06 19:08:59 +0530

    Created Bottom Tab Navigation

---
+++ Screens/Addmed.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Addmed = () => {
+  return (
+    <View>
+      <Text>Addmed</Text>
+    </View>
+  )
+}
+
+export default Addmed
---
+++ Screens/Progress.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Progress = () => {
+  return (
+    <View>
+      <Text>Progress</Text>
+    </View>
+  )
+}
+
+export default Progress
