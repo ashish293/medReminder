07d7ea34c8fa2553de40123b21eff1034eb30b99
Anuj Singh <101380771+anuj1010@users.noreply.github.com>
2022-04-06 12:32:54 +0530

    test commit

--- App.js
+++ App.js
@@ -72,7 +72,7 @@ const App: () => Node = () => {
           }}>
           <Section title="Step One">
             Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
+            screen and then come to see your edits.
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
