b3d38148175304a564078e4b530cab120cb1a898
Sandeep Jaiswal <sandeepj@eightarrows.io>
2022-04-06 23:21:07 +0530

    Stack icon

--- App.js
+++ App.js
@@ -1,32 +1,50 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {View, Text, StyleSheet} from 'react-native';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Home from './Screens/Home';
 import Addmed from './Screens/Addmed';
 import All from './Screens/All';
-import Progress from './Screens/Progress'; 
-
+import Progress from './Screens/Progress';
+import HomeIcon from './assets/icons/Home.svg';
+import AddIcon from './assets/icons/Add.svg';
+import AllIcon from './assets/icons/All.svg';
+import CompleteIcon from './assets/icons/Complete.svg';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const App = () => {
-  
   const Tab = createBottomTabNavigator();
 
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name='Home' component={Home} options={{headerShown:false}} />
-        <Tab.Screen name='Add' component={Addmed}/>
-        <Tab.Screen name='All' component={All}/>
-        <Tab.Screen name='Progress' component={Progress}/>
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarIcon: ({focused, color, size}) => {
+            switch (route.name) {
+              case 'Home':
+                return <HomeIcon color={focused ? '#3CC027' : 'black'} />;
+              case 'Add':
+                return <AddIcon color={focused ? 'black' : 'black'} />;
+              case 'All':
+                return <AllIcon color={focused ? 'black' : 'black'} />;
+              case 'Progress':
+                return <CompleteIcon color={focused ? 'black' : 'black'} />;
+              default:
+                return <AddIcon color={focused ? 'black' : 'black'} />;
+            }
+          },
+        })}>
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{headerShown: false}}
+        />
+        <Tab.Screen name="Add" component={Addmed} />
+        <Tab.Screen name="All" component={All} />
+        <Tab.Screen name="Progress" component={Progress} />
       </Tab.Navigator>
     </NavigationContainer>
-
-    
-
-   
   );
 };
 
-
-export default App
+export default App;
--- Screens/Home.js
+++ Screens/Home.js
@@ -1,40 +1,39 @@
-import { View, Text, StyleSheet, Image } from 'react-native';
+import {View, Text, StyleSheet, Image} from 'react-native';
 import React from 'react';
 import color from '../assets/constants/color';
 
 const Home = () => {
-
-
-
   return (
     <View style={styles.mainContainer}>
-     <View style={styles.headerContainer(color)}> 
-     <Image style={styles.icon} source={require('../assets/images/patient.png')}/><Text style={styles.headerText}>Today's Schedule</Text>
-     </View>
+      <View style={styles.headerContainer(color)}>
+        <Image
+          style={styles.icon}
+          source={require('../assets/images/patient.png')}
+        />
+        <Text style={styles.headerText}>Today's Schedule</Text>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  mainContainer:{
-  },
-  headerContainer:(color)=>({
-    flexDirection:'row',
-    backgroundColor:color.primary,
-    alignItems:'center',
+  mainContainer: {},
+  headerContainer: color => ({
+    flexDirection: 'row',
+    backgroundColor: color.primary,
+    alignItems: 'center',
   }),
-  icon:{
-    height:40,
-    width:40,
-    margin:5,
-    marginLeft:10,
+  icon: {
+    height: 40,
+    width: 40,
+    margin: 5,
+    marginLeft: 10,
   },
-  headerText:{
-     marginLeft:20,
-     fontSize:25,
-     color:'black',
+  headerText: {
+    marginLeft: 20,
+    fontSize: 25,
+    color: 'black',
   },
 });
 
-
-export default Home
+export default Home;
