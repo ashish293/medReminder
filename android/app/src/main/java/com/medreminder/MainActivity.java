fa6db523de3338ad1d88506bf7400ae570b7ab01
Ashish Kumar Burnwal <ashishburnwal293@gmail.com>
2022-04-11 16:58:04 +0530

    first build

--- android/app/src/main/java/com/medreminder/MainActivity.java
+++ android/app/src/main/java/com/medreminder/MainActivity.java
@@ -1,4 +1,4 @@
-package com.medreminder;
+package com.medicinereminder;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -7,16 +7,18 @@ import com.facebook.react.ReactRootView;
 public class MainActivity extends ReactActivity {
 
   /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * Returns the name of the main component registered from JavaScript. This is
+   * used to schedule
    * rendering of the component.
    */
   @Override
   protected String getMainComponentName() {
-    return "medReminder";
+    return "medicinereminder";
   }
 
   /**
-   * Returns the instance of the {@link ReactActivityDelegate}. There the RootView is created and
+   * Returns the instance of the {@link ReactActivityDelegate}. There the RootView
+   * is created and
    * you can specify the rendered you wish to use (Fabric or the older renderer).
    */
   @Override
